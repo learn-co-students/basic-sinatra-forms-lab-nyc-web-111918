9d87e47d27097087e5c86f8c3809311276ac7265
josh-sea <joshua.cocciardi@gmail.com>
2018-12-11 14:40:36 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,21 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+  get '/newteam' do
+    erb :newteam
+  end #gets newteam page with form
+
+  post '/newteam' do
+    #this takes form input
+  @name = params["name"]
+  @coach = params["coach"]
+  @pg = params["pg"]
+  @sg = params["sg"]
+  @pf = params["pf"]
+  @sf = params["sf"]
+  @c = params["c"]
+  erb :team #this passes instances to team
+  end
+
+
 end
